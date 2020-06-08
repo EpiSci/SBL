e0daacd8f7de562188e08855bfdd07f59ac3b8e2
Andrew Wilhelm <andrewwilhelm@ucla.edu>
2020-06-08 11:55:59 -0700

    Implemented Algorithm 15 of Collins' dissertation

--- collins.py
+++ collins.py
@@ -1,11 +1,36 @@
 import numpy as np
 import copy
 
+#Helper class to be used with the trie in the model
+class TrieNode():
+    def __init__(self,value=None,lfs=[]):
+        self.leaves = lfs
+        self.val = value
+
+#Given the head of a trie, find the largest consistent sequence that match one or more of the paths along the trie (See Figure 6.9)
+def largestConsistentSequence(head,sequence):
+    if not not sequence:
+        for leaf in head.leaves:
+            if leaf.val == sequence[0]:
+                list =  largestConsistentSequence(leaf,sequence[1:])
+                list.insert(0,leaf.val)
+                return list
+
+    #If it gets to this point, either head has no leaves or the sequence doesn't match
+    #Either way, return an empty list
+    return []
+        
+
 class CollinsModel():
     #Algorithm 11: Initialize sPOMDP model
     def __init__(self, environment, firstObservation):
         self.env = environment
-        #Note: not using a trie; instead the model's SDEs can be used
+        #Initialize the trie
+        leaves = []
+        for o in self.env.O_S:
+            leaves.append(TrieNode(o))
+        self.trieHead = TrieNode(None,leaves)
+        
         #The instance variable self.env has a current model associated with it. Thus lines 5 through 14 are unnecessary (lines 12 and 13 will be addressed below).
         #Note: lines 12 and 13 set the belief state to be 1 at the current observation
         self.beliefState = np.zeros([1,len(env.O_S)])
@@ -67,7 +92,7 @@ def updateModelParameters(model, action, prevOb, nextOb):
     maxOutcomeLength = max([len(sde) for sde in model.env.SDE_Set])
     if len(history) > maxOutcomeLength + 6:
         # Algorithm 15
-        model.beliefHistory = smoothBeliefHistory(history, model.beliefHistory)
+        model.beliefHistory = smoothBeliefHistory(model,history, model.beliefHistory)
         # Algorithm 16
         updateTransitionFunctionPosteriors(a, nextOb, model.beliefHistory)
         # Algorithm 17
@@ -87,7 +112,7 @@ def updateBeliefState(model, b, a, o):
     joint = np.zeros([len(b),len(b)])
     for m in range(len(b)):
         for m_prime in range(len(b)):
-            joint[m][m_prime] = model.T[m][a_index][m_prime]*b[m]
+            joint[m][m_prime] = model.T[m][a_index][m_prime]*b[m] #This line will throw an error. We need to use the Dirichlet distributions since we don't formally have a T matrix
 
     b_prime = [0 for val in range(m)]
     for m_prime in range(len(b)):
@@ -107,3 +132,36 @@ def updateBeliefState(model, b, a, o):
 
 
             
+#Algorithm 15: Smooth Belief History
+def smoothBeliefHistory(model, history, beliefHistory):
+    for i in range(3):
+        savedBeliefs = copy.deepcopy(beliefHistory[i])
+        largestMatching = largestConsistentSequence(model.trieHead,history[2*i:])
+        matching = [sde for sde in model.env.SDE_Set if sde[0:len(largestMatching)] == largestMatching] #Only include those SDEs that contain thte largestMatching sequence at their beginning
+        beliefHistory[i] = [0 for val in range(len(beliefHistory[i]))]
+        for match in matching:
+            matchingState = model.env.SDE_Set.index(match)
+            beliefHistory[i][matchingState] = savedBeliefs[matchingState]
+
+        total = 0
+        for m in len(model.env.SDE_Set):
+            total = total + beliefHistory[i][m]
+        for m in len(model.env.SDE_Set):
+            beliefHistory[i][m] = beliefHistory[i][m] / total
+
+    return beliefHistory
+            
+        
+
+
+# print("hi")
+# n8 = TrieNode("diamond",[])
+# n7 = TrieNode("square",[])
+# n6 = TrieNode("diamond",[])
+# n5 = TrieNode("x",[n7,n8])
+# n4 = TrieNode("y",[n6])
+# n3 = TrieNode("diamond",[n5])
+# n2 = TrieNode("square",[n4])
+# n1 = TrieNode(None, [n2,n3])
+# print(largestConsistentSequence(n1,["square","y","diamond"]))
+# print(largestConsistentSequence(n1,["chartrues"]))
