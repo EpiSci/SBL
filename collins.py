0725b396cc77c69d95c301d164257a4201a255ae
Andrew Wilhelm <andrewwilhelm@ucla.edu>
2020-06-10 14:49:36 -0700

    Implemented Algorithm 18 of Collins' dissertation.

--- collins.py
+++ collins.py
@@ -1,14 +1,15 @@
 import numpy as np
 import copy
 
-#Helper class to be used with the trie in the model
+
+# Helper class to be used with the trie in the model
 class TrieNode():
-    def __init__(self,value=None,lfs=[]):
+    def __init__(self, value=None, lfs=[]):
         self.leaves = lfs
         self.val = value
 
-#Given the head of a trie, find the largest consistent sequence that match one or more of the paths along the trie (See Figure 6.9)
-def largestConsistentSequence(head,sequence):
+# Given the head of a trie, find the largest consistent sequence that match one or more of the paths along the trie (See Figure 6.9)
+def largestConsistentSequence(head, sequence):
     if not not sequence:
         for leaf in head.leaves:
             if leaf.val == sequence[0]:
@@ -16,8 +17,8 @@ def largestConsistentSequence(head,sequence):
                 list.insert(0,leaf.val)
                 return list
 
-    #If it gets to this point, either head has no leaves or the sequence doesn't match
-    #Either way, return an empty list
+    # If it gets to this point, either head has no leaves or the sequence doesn't match
+    # Either way, return an empty list
     return []
 
 def insertSequence(head,sequence):
@@ -26,7 +27,7 @@ def insertSequence(head,sequence):
             if leaf.val == sequence[0]:
                 insertSequence(leaf,sequence[1:])
                 return None
-        #If it gets here, then need to insert a new node
+        # If it gets here, then need to insert a new node
         newNode = TrieNode(sequence[0])
         head.leaves.append(newNode)
         insertSequence(newNode,sequence[1:])
@@ -34,10 +35,10 @@ def insertSequence(head,sequence):
         
 
 class CollinsModel():
-    #Algorithm 11: Initialize sPOMDP model
-    def __init__(self, environment, firstObservation):
+    # Algorithm 11: Initialize sPOMDP model
+    def __init__(self, environment, firstObservation, minimumGain):
         self.env = environment
-        #Initialize the trie
+        # Initialize the trie
         leaves = []
         for o in self.env.O_S:
             leaves.append(TrieNode(o))
@@ -45,26 +46,41 @@ class CollinsModel():
         
         #The instance variable self.env has a current model associated with it. Thus lines 5 through 14 are unnecessary (lines 12 and 13 will be addressed below).
         #Note: lines 12 and 13 set the belief state to be 1 at the current observation
-        self.beliefState = np.zeros([1,len(env.O_S)])
-        self.beliefState[env.O_S.index(firstObservation)] = 1
+        self.beliefState = np.zeros([1,len(self.env.O_S)])
+        self.beliefState[self.env.O_S.index(firstObservation)] = 1
         
-        #Note: self.TCounts is of shape (a,m,m') and not (m,a,m') for efficiency
-        self.TCounts = np.ones((len(env.A_S),len(SDE_List),len(SDE_List)))
+        # Note: self.TCounts is of shape (a,m,m') and not (m,a,m') for efficiency
+        self.TCounts = np.ones((len(self.env.A_S),len(self.env.SDE_Set),len(self.env.SDE_Set)))
         #Note: using a (a,a',m,m',m'') matrix instead of a counter list for efficiency
-        self.OneTCounts = np.ones((len(env.A_S),len(env.A_S),len(SDE_List),len(SDE_List),len(SDE_List)))
+        self.OneTCounts = np.ones((len(self.env.A_S),len(self.env.A_S),len(self.env.SDE_Set),len(self.env.SDE_Set),len(self.env.SDE_Set)))
         #Note: not using M.T, M.OneT, and Algorithm 12 as these can be determined more efficiently by using dirichlet distributions
         self.actionHistory = []
         self.observationHistory = []
         self.observationHistory.append(firstObservation)
         self.beliefHistory = []
         self.beliefHistory.append(copy.deepcopy(self.beliefState))
+        self.minGain = minimumGain
+
+    # Reinitialize a model (after the new SDEs have been inserted)
+    def reinitializeModel(self):
+        self.TCounts = np.ones((len(self.env.A_S),len(self.env.SDE_Set),len(self.env.SDE_Set)))
+        self.OneTCounts = np.ones((len(self.env.A_S),len(self.env.A_S),len(self.env.SDE_Set),len(self.env.SDE_Set),len(self.env.SDE_Set)))
         
+        self.beliefState = np.zeros([1,len(self.env.O_S)])
+        sdeFirstObservations = [sde[0] for sde in self.env.SDE_Set]
+        self.beliefState[sdeFirstObservations.index(self.environment.get_observation())] = 1
+        self.beliefState = self.beliefState / np.sum(beliefState)
+        self.beliefHistory = []
+        self.beliefHistory.append(copy.deepcopy(self.beliefState))
+        self.actionHistory = []
+        self.observationHistory = []
+
         
 
 # Algorithm 10: PSBL Learning of SPOMDP Models
-def psblLearning(env, numActions, explore, patience):
+def psblLearning(env, numActions, explore, patience,minGain):
     prevOb = env.reset()
-    model = CollinsModel(env,prevOb)
+    model = CollinsModel(env,prevOb,minGain)
     minSurpriseModel = None
     minSurprise = float("inf")
     splitsSinceMin = 0
@@ -90,6 +106,7 @@ def psblLearning(env, numActions, explore, patience):
             splitsSinceMin = 1
         if splitsSinceMin > patience:
             break
+        #Algorithm 18
         foundSplit = trySplit(model)
     return minSurpriseModel
 
@@ -200,4 +217,82 @@ def updateOneStepFunctionPosteriors(model, history):
                 totalCounts = totalCounts + counts[m_idx][mp_idx][mdp_idx]
 
     #Note: Not necessary to do updateOneStepProbabilities (analagous to Algorithm 12) since this is handled by the dirichlet distributions
+
+
+# Algorithm 18: sPOMDP Model State Splitting
+def trySplit(model):
+    G_ma = computeGains(model)
+    G = []
+    # Generate the list G that is used to order the model splitting
+    mTrajLengths = [len(sde) for sde in model.env.SDE_Set]
+    sortedIndexes = sorted(range(len(mTrajLengths),key= mTrajLengths.__getitem__))
+    for m in sortedIndexes:
+        for a in model.env.A_S:
+            # Note: These are only sorted according to the length of m.trajectory, as described in the algorithm (i.e. the sorting with respect to the action a is arbitrary)
+            G.append(((model.env.SDE_Set[m],model.env.A_S[a]),G_ma[a][m]))
+
+    for gs in G:
+        state = gs[0][0]
+        action = gs[0][1]
+        gainValue = gs[1]
+        if gainValue > model.minGain:
+            #Set m1 and m2 to be the two most likely states that are transitioned into from state m taking action a
+            m_index = model.env.SDE_Set.index(state)
+            a_index = model.env.A_S.index(action)
+            transitionSetProbs = dirichlet.mean(model.TCounts[a_index, m_idx, :])
+            orderedVals = copy.deepcopy(transitionSetProbs)
+            orderedVals.sort()
+            prob1 = orderedVals[-1] #largest probability
+            prob2 = orderedvals[-2] #second largest probability
+            sde1_idx = np.where(transitionSetProbs == prob1)[0][0]
+            sde2_idx = np.where(transitionSetProbs == prob2)[0][0]
+            m1 = env.get_SDE()[sde1_idx]
+            m2 = env.get_SDE()[sde2_idx]
+
+            newOutcome1 = copy.deepcopy(m1)
+            newOutcome1.insert(0,action)
+            newOutcome1.insert(0,state[0])
+
+            newOutcome2 = copy.deepcopy(m2)
+            newOutcome2.insert(0,action)
+            newOutcome2.insert(0,state[0])
+
+            outcomesToAdd = []
+            if newOutcome1 not in model.env.SDE_Set:
+                outcomesToAdd.append(newOutcome1)
+                insertSequence(model.trieHead,newOutcome1)
+
+            if newOutcome2 not in model.env.SDE_Set:
+                outcomesToAdd.append(newOutcome2)
+                insertSequence(model.trieHead,newOutcome2)
+
+            #Note: Not updating model.MaxOutcomeLength as this is generated dynamically when needed in Algorithm 13
+            if len(outcomesToAdd) > 1:
+                # Note: The modelState class is not used in this implementation so making a new modelState instance is not necessary.
+
+                model.env.SDE_Set.add(newOutcome1)
+                model.env.SDE_Set.add(newOutcome2)
+                model.reinitializeModel()
+                return True
+            
+            elif len(outcomesToAdd) == 1:
+                model.env.SDE_Set.add(outcomesToAdd[0])
+                model.reinitializeModel()
+                return True
+            
+    return False
+
+# Compute gains according to equation 6.10 (Helper Function for Algorithm 18)
+def computeGains(model):
+    G = np.zeros([len(model.env.A_S),len(model.env.SDE_Set)])
+
+    for mp in range(len(model.env.SDE_Set)):
+        for ap in range(len(model.env.SDE_Set)):
+            sum = 0
+            for m in range(len(model.env.SDE_Set)):
+                for a in range(len(model.env.SDE_Set)):
+                    w_ma = (dirichlet.mean(model.TCounts[a, m, :])[mp])
+                    sum = sum + (w_ma * entropy((dirichlet.mean(model.OneTCounts[a, ap, m, mp, :])), base=len(model.env.SDE_Set)))
+            G[ap][mp] = entropy((dirichlet.mean(model.TCounts[ap, mp, :])), base=len(model.env.SDE_Set)) - sum
+    return G
 
